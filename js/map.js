00bd7d079fc5f9367c9e4c7756dc8d2669308472
Midori IT Office, LLC <13025500+midoriit@users.noreply.github.com>
2021-07-10 07:59:31 +0900

    add files

--- js/map.js
+++ js/map.js
@@ -11,20 +11,53 @@
 
   layer.addTo(map);
 
+  var icon_color = ['red', 'darkred', 'lightred', 'orange', 'beige', 'green',
+                    'darkgreen', 'lightgreen', 'blue', 'darkblue', 'lightblue',
+                    'purple', 'darkpurple', 'pink', 'cadetblue', 'gray', 'lightgray'];
+
+  var current_7 = '';
+  var icon_n = 0;
+
   $.getJSON( 'poi.geojson', function(data) {
     var poiLayer = L.geoJson(data, {
       pointToLayer: function (feature, latlng) {
+
+        Object.keys(feature.properties).forEach( function(k){
+          if( k == '七福神めぐりの名称' ) {
+            if( feature.properties[k] != current_7 ) {
+              current_7 = feature.properties[k];
+              icon_n = Math.floor( Math.random() * icon_color.length );
+            }
+          }
+        });
+
         return L.marker(latlng, {
+          icon: L.AwesomeMarkers.icon({
+            prefix: 'fa',
+            icon: 'circle-o',
+            markerColor: icon_color[icon_n]
+          })
         }).bindPopup(
           function() {
             tr = '<table border>';
             Object.keys(feature.properties).forEach( function(k){
-              tr = tr + 
+              var v = feature.properties[k];
+              if( typeof(v) == 'string' && v.indexOf('http') === 0) {
+                tr = tr + 
+                   '<tr><td style="white-space: nowrap;">' + 
+                   k + 
+                   '</td><td style="white-space: nowrap;">' +
+                   '<a href="' + v + '" target="_blank">' + 
+                   v + '</a>' +
+                   '</td></tr>';
+              } else if ( typeof(v) == 'string' && v != '' ) {
+                tr = tr + 
                    '<tr><td style="white-space: nowrap;">' + 
                    k + 
                    '</td><td style="white-space: nowrap;">' +
                    feature.properties[k] + 
                    '</td></tr>';
+              }
             });
             return tr + '</table>';
           }
